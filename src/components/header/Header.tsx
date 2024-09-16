723aa44daff4d90240ac8bc010641773c3dbdb01
Grzegorz Kunc <Grzegorz.Kunc@GAINCapital.com>
2024-09-16 07:26:43 +0200

    Working on moble menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,18 +1,28 @@
 import './Header.css';
+import Menu from './Menu';
 
 function Header() {
   return (
-    <header className='header'>
-      <div className='header__logo'>
-        GKUNC
-      </div>
-      <div className='header__navigation'>
-        <a className='header__link'>Home</a>
-        <a className='header__link'>About</a>
-        <a className='header__link'>Contact</a>
-      </div>
-      <button  className='header__cta'>Contact Us!</button>
-    </header>
+    <>
+      <header className='header'>
+        <div className='header__logo'>
+          GKUNC
+        </div>
+        <div className='header__navigation'>
+          <a className='header__link'>Home</a>
+          <a className='header__link'>About</a>
+          <a className='header__link'>Contact</a>
+        </div>
+        <button  className='header__cta'>Contact Us!</button>
+      </header>
+
+      <header className='header__mobile'>
+        <div className='header__logo'>
+          GKUNC
+        </div>
+        <Menu />
+      </header>
+    </>
   );
 }
 
